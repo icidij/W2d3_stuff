d87f9a78a9a435d96ff7fc0b96db3b1bc1718cab
icidij <jamesc1288@gmail.com>
2021-08-18 17:34:54 -0400

    ughhh so slow

--- mastermind_project/mastermind_project/lib/code.rb
+++ mastermind_project/mastermind_project/lib/code.rb
@@ -1,9 +1,119 @@
 class Code
+
+attr_accessor :pegs
+
   POSSIBLE_PEGS = {
     "R" => :red,
     "G" => :green,
     "B" => :blue,
     "Y" => :yellow
   }
+  def self.valid_pegs?(arr)
+    arr.all? {|item| POSSIBLE_PEGS.include?(item.upcase)}
+  end
+  def initialize(arr)
+    raise "Error" if !Code.valid_pegs?(arr)
+    @pegs = arr.map(&:upcase)
+
+  end
+
+
+
+  def self.random(num)
+     Code.new(Array.new(num, POSSIBLE_PEGS.keys.sample))
+    
+  end
+
+
+
+  def self.from_string(string)
+    # a = Code.new(Array.new(string.length, ["R", "G", "B", "Y"].sample))
+
+    # string.each_char.with_index {|char, i| a.pegs[i] = char.upcase}
+
+    # a
+    Code.new(string.split(""))
+
+
+
+
+
+  end
+
+
+  def [](index)
+
+   @pegs[index]
+  end
+
+
+  def length
+
+    @pegs.length
+  end
+
+
+  def num_exact_matches(guess)
+    count = 0
+    guess.pegs.each_with_index {|spot, i| count += 1 if spot == @pegs[i]}
+    count
+
+
+  end
+
+
+
+
+
+  def num_near_matches(guess)
+    near_matches = 0
+    unmatched_pegs = []
+    unmatched_guess_pegs = []
+
+    guess.pegs.each_with_index do |spot, i| 
+      
+      if !(spot == @pegs[i])
+        unmatched_pegs << @pegs[i]
+
+        unmatched_guess_pegs << spot 
+
+        
+      end
+      unmatched_pegs
+      unmatched_guess_pegs
+    end
+
+    
+    
+
+
+
+    upcount = Hash.new(0)
+    ugpcount = Hash.new(0)
+    unmatched_guess_pegs.each {|color| ugpcount[color] += 1}
+    
+    unmatched_pegs.each {|color| upcount[color] += 1}
+
+    upcount.each {|k, v| near_matches += 1 if ugpcount.has_key?(k)}
+
+
+  near_matches
+end
+
+def ==(arg)
+
+  return false if @pegs.length != arg.pegs.length
+  
+  
+  
+  self.pegs.each_with_index do |item, i|  
+    if !(item == arg.pegs[i])
+      return false
+    end
+
+  end
+true
+end
+
 
 end
--- mastermind_project/mastermind_project/lib/mastermind.rb
+++ mastermind_project/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,38 @@
 require_relative "code"
-
 class Mastermind
+attr_accessor :secret_code
+
+    def initialize(num)
+
+        @secret_code = Code.random(num)
+       
+    end
+
+
+    def print_matches(arg)
+        q = arg.pegs
+        count = 0
+        @secret_code.pegs.each_with_index {|item, i| count += 1 if item == q[i]}
+        p count
+
+        r = @secret_code.num_near_matches(arg)
+        p r
+        # s = Code.from_string(r).pegs
+        # p s #r.Code.num_exact_matches(q)
+    
+    
+    
+    end
+    def ask_user_for_guess
+        
+
+
+
+
+
+
+
+
+
 
 end
